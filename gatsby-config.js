bdd6212182da7317015b4b75a2ee0be3bb8f099d
Miłosz Sobczak <kontakt@miloszsobczak.pl>
2020-10-08 12:43:21 +0200

    chore: update GA code

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-180119421-1`,
       },
     },
     `gatsby-plugin-feed`,
